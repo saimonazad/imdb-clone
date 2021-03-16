db7196164fd5f3116deccda523ff01456da277e6
Alauddin Al Azad <saimon.azad@hotmail.com>
2021-03-16 14:11:40 +0600

    header slider functionality added

---
+++ src/CardHeader.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+const CardHeader = ({ movies }) => {
+  let size = 3;
+  const renderedItem = movies
+    .slice(0, size)
+    .map(({ title, time, url, image }) => {
+      return (
+        <React.Fragment>
+          <div className="flex flex-row p-3 shadow  ">
+            <div className="">
+              <img src={image} width="85" />
+            </div>
+            <div className="">
+              <svg
+                className="text-white w-12 h-12 ml-2 inline"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
+                />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+              <span className="text-white ml-2 align-bottom">{time}</span>
+              <h1 className="text-white ml-4 text-lg">{title}</h1>
+              <p className="text-gray-400 ml-4 text-sm">Watch the Trailer</p>
+            </div>
+          </div>
+        </React.Fragment>
+      );
+    });
+
+  return <>{renderedItem}</>;
+};
+
+export default CardHeader;
---
+++ src/Header.js
@@ -0,0 +1,204 @@
+import React, { useState } from "react";
+import "./Header.css";
+import ListHeader from "./ListHeader";
+
+const headerListMovies = [
+  {
+    title: "'Zack Snyder's Justice League'",
+    time: "2:19",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BYjI3NDg0ZTEtMDEwYS00YWMyLThjYjktMTNlM2NmYjc1OGRiXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_QL75_UX280_CR0,0,280,414_.jpg",
+    url: "#",
+    featuredImage:
+      "https://m.media-amazon.com/images/M/MV5BMzc5Zjc2YTMtM2IwZC00OTAyLThjZTktNGU3MjVjNTY5YThlXkEyXkFqcGdeQXNuZXNodQ@@._V1_QL75_UX1000_CR0,0,1000,563_.jpg",
+  },
+  {
+    title: "The Falcon and the Winter Soldier",
+    time: "1:19",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BODNiODVmYjItM2MyMC00ZWQyLTgyMGYtNzJjMmVmZTY2OTJjXkEyXkFqcGdeQXVyNzk3NDUzNTc@._V1_QL75_UX280_CR0,0,280,414_.jpg",
+    url: "#",
+    featuredImage:
+      "https://m.media-amazon.com/images/M/MV5BOGQ4NmEwZDMtNWNjNy00Mjc4LThmZTMtYmIxYjU3ZmEyMWY5XkEyXkFqcGdeQW1hcmNtYW5u._V1_QL75_UX1000_CR0,0,1000,563_.jpg",
+  },
+  {
+    title: "The Rise of Henry Cavill",
+    time: "4:03",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BOWY2OGY3MjktMDA1Ny00NDJkLTllMTEtMWVhNjg1MWQ2YmQ3XkEyXkFqcGdeQXVyNDYzNjU3ODM@._V1_QL75_UX280_CR0,0,280,414_.jpg",
+    url: "#",
+    featuredImage:
+      "https://m.media-amazon.com/images/M/MV5BNjhjZWU5OTgtYzg5ZC00MjUyLTk3ZTItMjUzYmEzYWMyYjU5XkEyXkFqcGdeQXNoYW5nZWxs._V1_QL75_UX1000_CR0,0,1000,563_.jpg",
+  },
+  {
+    title: "In the Heights",
+    time: "2:24",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BZDkzYmM5M2EtMjQxMS00ZDNiLTkwOWQtOTExZDdiMDIxNmQxXkEyXkFqcGdeQXVyNjY1MTg4Mzc@._V1_QL75_UX280_CR0,0,280,414_.jpg",
+    url: "#",
+    featuredImage:
+      "https://m.media-amazon.com/images/M/MV5BOGZhNjcxM2ItODRlZC00ODhkLWJlMjgtYzcyMWMxYzI0NTkyXkEyXkFqcGdeQXNuZXNodQ@@._V1_QL75_UX1000_CR0,6,1000,563_.jpg",
+  },
+  {
+    title: "From 'Suicide Squad' to 'Ma Rainey's Black Bottom'",
+    time: "2:40",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BNTBlZGY1OTAtN2RjMC00ZThiLWFiZmUtN2VkOGMxNmMyYjQwXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_QL75_UX280_CR0,0,280,414_.jpg",
+    url: "#",
+    featuredImage:
+      "https://m.media-amazon.com/images/M/MV5BNjVlYTIzMGQtYTcyZS00NTI3LTg3YTktOTFjZjY1ZGZkZDJhXkEyXkFqcGdeQW1pYnJ5YW50._V1_QL75_UX1000_CR0,0,1000,563_.jpg",
+  },
+  {
+    title: "'Cruella'",
+    time: "1:01",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BZWQ1YjNiMTItZjkwZS00NDMyLThiOWEtNzBkMzAzMWU2ZDUwXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_QL75_UX280_CR0,0,280,414_.jpg",
+    url: "#",
+    featuredImage:
+      "https://m.media-amazon.com/images/M/MV5BZGRhMDVhYWQtZjExNi00ZDEzLWI4ZDItNGIzYWE1Y2Q4MDNiXkEyXkFqcGdeQWpnYW1i._V1_QL75_UX1000_CR0,0,1000,563_.jpg",
+  },
+];
+
+const Header = () => {
+  const [activeSlideIndex, setactiveSlideIndex] = useState(0);
+
+  const onClickHandler = (type) => {
+    const numberOfSlides = headerListMovies.length;
+    let currentSlideIndex = activeSlideIndex;
+    let nextSlideIndex = undefined;
+    switch (type) {
+      case "prev":
+        nextSlideIndex = currentSlideIndex;
+        if (currentSlideIndex > 0) {
+          nextSlideIndex = currentSlideIndex - 1;
+        } else {
+          nextSlideIndex = numberOfSlides - 1;
+        }
+        break;
+      case "next":
+        nextSlideIndex = currentSlideIndex;
+        if (currentSlideIndex < numberOfSlides - 1) {
+          nextSlideIndex = currentSlideIndex + 1;
+        } else if (currentSlideIndex === numberOfSlides - 1) {
+          currentSlideIndex = -1;
+          nextSlideIndex = currentSlideIndex + 1;
+        }
+    }
+    if (nextSlideIndex !== undefined) {
+      setactiveSlideIndex(nextSlideIndex);
+    }
+    console.log(activeSlideIndex);
+  };
+
+  return (
+    <header className="bg-black">
+      <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+        <div className="flex ">
+          <div className="flex-auto w-8/12">
+            <div className="relative">
+              <div
+                className="test bg-no-repeat bg-cover h-full w-full"
+                style={{
+                  minHeight: "500px",
+                  backgroundImage: `linear-gradient(\n      rgba(0, 0, 0, 0) 0%,\n      rgba(0, 0, 0, 0) 60%,\n      rgba(0, 0, 0, 0.2) 70%,\n      rgba(0, 0, 0, 0.7) 80%,\n      rgba(0, 0, 0, 0.9) 90%,\n      rgba(0, 0, 0, 1) 100%\n    ),\n    url(${headerListMovies[activeSlideIndex].featuredImage})`,
+                }}
+              ></div>
+              <div className="absolute bottom-0">
+                <div className="flex">
+                  <div className="flex-auto ml-3 shadow-2xl">
+                    <img
+                      alt="#204 - Henry Cavill"
+                      loading="lazy"
+                      src={`${headerListMovies[activeSlideIndex].image}`}
+                      width="140"
+                    />
+                  </div>
+                  <div className="flex-auto self-end">
+                    <div className="flex">
+                      <div className="flex-auto w-24 text-white px-2">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
+                          />
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                          />
+                        </svg>
+                      </div>
+                      <div className="flex-auto self-center">
+                        <div className="flex">
+                          <h1 className="text-4xl text-white inline px-4 leading-9">
+                            {headerListMovies[activeSlideIndex].title}
+                          </h1>
+                          <span className="text-white px-4 self-end">
+                            {headerListMovies[activeSlideIndex].time}
+                          </span>
+                        </div>
+                        <p className="text-white px-4">
+                          Watch The Trailer!
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div
+                onClick={() => onClickHandler("prev")}
+                className="absolute top-1/3 left-0 text-white w-14 h-14 p-3 bg-black border bg-opacity-50 border-white rounded cursor-pointer hover:text-yellow-400 font-bold border"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M15 19l-7-7 7-7"
+                  />
+                </svg>
+              </div>
+              <div
+                onClick={() => onClickHandler("next")}
+                className=" absolute top-1/3 right-0 text-white w-14 h-14 p-3 bg-black border bg-opacity-50 border-white rounded cursor-pointer hover:text-yellow-400 font-bold border"
+              >
+                <svg
+                  className="align-middle"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M9 5l7 7-7 7"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div className="flex-auto w-4/12">
+            <ListHeader movies={headerListMovies} />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
